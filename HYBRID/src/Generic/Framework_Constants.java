3b0ee4a606572c5cd53df267bc64e72493f96d40
CHETHAN <CHETHAN@LAPTOP-I4IEUNTM>
2022-07-07 16:26:04 +0530

    Project1

---
+++ HYBRID/src/Generic/Framework_Constants.java
@@ -0,0 +1,12 @@
+package Generic;
+
+public interface Framework_Constants {
+	
+	String Gecko_Key="webdriver.gecko.driver";
+	String Gecko_Value="./Softwares/geckodriver.exe";
+	String Chrome_Key="webdriver.chrome.driver";
+	String Chrome_Value="./Softwares/geckodriver.exe";
+	
+	String Base_Url="htpps://www.facebook.com";
+
+}
